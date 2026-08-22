df7adb35505058366f960244d10528627b5e16c2
Sarcon Ulama <sarconulama@yahoo.com>
2026-08-22 16:06:17 +0800

    added ssome background but for some reason its not full screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import DotGrid from "./components/ui/DotGrid";
+
 function App() {
   return (
-    <div>
-      <h1>My Portfolio</h1>
-    </div>
+    <main className="fixed inset-0 w-screen h-screen overflow-hidden bg-[#0f0d13]">
+      <DotGrid
+        dotSize={5}
+        gap={15}
+        baseColor="#2F293A"
+        activeColor="#f71010"
+        proximity={120}
+        shockRadius={250}
+        shockStrength={5}
+        resistance={750}
+        returnDuration={1.5}
+      />
+    </main>
   );
 }
 
