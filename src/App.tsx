e8f3dd06e53ada113ba5b9c2b1925721cfef2cae
Sarcon Ulama <sarconulama@yahoo.com>
2026-09-08 23:33:30 +0800

    not finished yet

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,33 @@ function App() {
   return (
     <main>
 
-   
-      
+      <section className="w-screen h-screen flex align-center">
+          <div className="w-full h-full relative">
+            <div className="NAME p-4">SARCON</div>
+            <nav className="">
+              <a href="#" className="hover:underline">HOME</a>
+              <a href="#" className="hover:underline">ABOUT</a>
+              <a href="#" className="hover:underline">WORKS</a>
+            </nav>
+          </div>
 
-      
 
-       
 
         {/* Lanyard */}
-        <div>
+        <div className="w-full h-full relative z-10">
           <Lanyard
             position={[0, 0, -20]}
             gravity={[0, -40, 0]}
           />
         </div>
+      </section>
+
+
+
+
+
+
+
 
 
 
